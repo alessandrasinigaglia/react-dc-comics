ea3be1c56f20800986abc1348b13de5fd2a02a89
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2025-01-28 19:49:20 +0100

    feat: add footer top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,31 @@
 export default function Footer() {
-    return <footer>Footer</footer>;
+    return <footer>
+        <div className="footer-top">
+            <div className="container">
+                <div className="infos">
+                    <div className="icons">
+                         <img src="/img/buy-comics-digital-comics.png" alt="Digital Comics"></img>
+                        <p>DIGITAL COMICS</p>
+                    </div>
+                    <div className="icons">
+                        <img src="/img/buy-comics-merchandise.png" alt="Merchandise"></img>
+                        <p>DC MERCHANDISE</p>
+                    </div>
+                    <div className="icons">
+                        <img src="/img/buy-comics-subscriptions.png" alt="Merchandise"></img>
+                        <p>SUBSCRIPTION</p>
+                    </div>
+                     <div className="icons">
+                        <img src="/img/buy-comics-shop-locator.png" alt="Merchandise"></img>
+                        <p>COMIC SHOP LOCATOR</p>
+                    </div>
+                    <div className="icons">
+                        <img src="/img/buy-dc-power-visa.svg" alt="Merchandise"></img>
+                        <p>DC POWER VISA</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer>;
 }
