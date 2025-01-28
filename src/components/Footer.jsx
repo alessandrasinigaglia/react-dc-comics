910de34d67001b4de6eb8918268fd6de5fe2e0f6
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2025-01-28 21:38:17 +0100

    feat: add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,6 +26,64 @@ export default function Footer() {
                 </div>
             </div>
         </div>
-
+        <section className="footer-mid">
+            <div className="container">
+                <div className="row-foot">
+                <div className="col">
+                    <h1>DC COMICS</h1>
+                    <p>Characters</p>
+                    <p>Comics</p>
+                    <p>Movies</p>
+                    <p>TV</p>
+                    <p>Games</p>
+                    <p>Videos</p>
+                    <p>News</p>
+                </div>
+                <div className="col">
+                    <h1>DC</h1>
+                    <p>Terms Of Use</p>
+                    <p>Privacy Policy (new)</p>
+                    <p>Ad Choices</p>
+                    <p>Advertising</p>
+                    <p>Jobs</p>
+                    <p>Subscriptions</p>
+                    <p>Talent Workshops</p>
+                    <p>CPSC Certificates</p>
+                    <p>Ratings</p>
+                    <p>Shop Help</p>
+                    <p>Contact Us</p>
+                </div>
+                <div className="col">
+                    <h1>SITES</h1>
+                    <p>DC</p>
+                    <p>MAD Magazine</p>
+                    <p>DC Kids</p>
+                    <p>DC Universe</p>
+                    <p>DC Power Visa</p>
+                </div>
+                <img src="/img/dc-logo-bg.png" alt="LOGO DC"></img>
+                </div>
+                <div className="col-bott">
+                    <h1>SHOP</h1>
+                    <p>Shop DC</p>
+                    <p>Shop DC Collectibles</p>
+                </div>
+            </div>
+        </section>
+        <section className="footer-bottom">
+            <div className="container">
+                <div className="row-bott">
+                <button className="btn">SIGN UP NOW !</button>
+                <div className="icons">
+                <h1>FOLLOW US</h1>
+                <img src="/img/footer-facebook.png"></img>
+                <img src="/img/footer-twitter.png"></img>
+                <img src="/img/footer-youtube.png"></img>
+                <img src="/img/footer-pinterest.png"></img>
+                <img src="/img/footer-periscope.png"></img>
+                </div>
+                </div>
+            </div>
+        </section>
     </footer>;
 }
