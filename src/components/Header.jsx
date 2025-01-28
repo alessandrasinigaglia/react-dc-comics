2a0815cab0e22a5950f6ef44fb6f13f873b6ad57
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2025-01-28 18:46:46 +0100

    feat: add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,63 @@
 export default function Header() {
-    return <header>Header</header>;
+    return (
+    <header className="container">
+        <div className="raw">
+        <img src="/img/dc-logo.png"></img>
+        <nav className="nav-list">
+            <ul>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+            </ul>
+        </nav>
+        </div>
+        {/* <div className="selector"></div> */}
+    </header>
+    
+    );
 }
